80cc0b2d28af597ea3e7627b5f3a6595636d1464
Raphael TISSERAND <rtissera@paul-f4Ar6s8.clusters.42paris.fr>
2024-10-01 20:43:16 +0200

    better path telescope

--- lua/plugins/telescope_config.lua
+++ lua/plugins/telescope_config.lua
@@ -8,7 +8,7 @@ function telescope_config_lua.setup()
 			prompte_prefix = "🔍 ",
 			selection_caret = "➜ ",
 			entry_prefix = "  ",
-			path_display = { "smart" },
+			path_display = { "shorten" },
 
 			mappings = {
 				i = {
