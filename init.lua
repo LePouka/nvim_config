61775a608ac27b1d46ce016220ffd4fe87fa19df
Raphael TISSERAND <rtissera@paul-f4Ar6s8.clusters.42paris.fr>
2024-10-01 16:27:13 +0200

    ouais

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
-
 require('core.options').setup()
 require('core.keymaps').setup()
 require('core.autocommands').setup()
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,11 @@
 local options = {}
 
 function options.setup()
+
+	vim.g.mapleader = ' '
+	vim.g.maplocalleader = ' '
+	vim.g.have_nerd_font = true
+
 	vim.opt.number = true
 	vim.opt.relativenumber = true
 	vim.opt.mouse = 'a'
