6b1558401fbecae31e4d76e52c2a88f13310d795
raph2leretour <tisserandraph@gmail.com>
2024-11-18 14:10:18 +0100

    -jk

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 local keymaps = {}
 
 function keymaps.utils()
-	vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
+	-- vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Exit insert mode' })
 	vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 	vim.keymap.set("n", "<leader>ex", ":Ex<CR>", { desc = "File [EX]plorer" })
 	vim.keymap.set("n", "<leader>h", ":Stdheader<CR>", { desc = "42 [H]eader" })
